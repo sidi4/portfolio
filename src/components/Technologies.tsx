ac7da22e0c39adcf6faafcff8f9e7ef3bd083370
SidiMohamed <118294090+sidi4@users.noreply.github.com>
2025-08-20 11:56:04 +0000

    Update Technologies.tsx

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -29,8 +29,8 @@ const Technologies = () => {
     <div className="min-h-screen flex items-center font-sans relative">
       <AnimatedBackground />
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full m-8 relative z-10">
-        <h1 className="text-4xl font-bold text-white mb-8 text-center">Technologies & Skills</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <h1 className="text-4xl font-bold text-white mb-8 text-center mt-12">Technologies & Skills</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {technologies.map((tech, index) => (
             <div 
               key={index}
